e22d0840cbc926aeb5aace28da42a8ad2119bb60
Sukhvinder Singh <krramsos@gmail.com>
2014-02-26 16:40:24 +0100

    Initial commit

---
+++ pfaGUI.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from functools import partial
+import Tkinter
+
+root = Tkinter.Tk()
+MyButton = partial(Tkinter.Button, root,
+	fg ='white', bg='blue')
+b1 = MyButton(text='Button1')
+b2 = MyButton(text='Button2')
+qb = MyButton(text='QUIT', bg='red',
+	command=root.quit)
+b1.pack()
+b2.pack()
+qb.pack(fill=Tkinter.X, expand=True)
+root.title('PFAs!')
+root.mainloop()
+
+
+
